d2b2e90837b7e1b90abef50299715fee2d3a1941
Fernando da Costa Felício <ffelicio@autodoc.com.br>
2018-08-31 17:03:28 -0300

    Inclusão do Adapter para a referência pro RecyclerView.

---
+++ app/src/main/java/com/example/recyclerviewproject/adapter/TaskListAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.recyclerviewproject.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    /**
+     * Nested Class (Classe aninhada)
+     * A mesma somente será utilizada para o Adapter TaskListAdapter
+     */
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
+
