020f63041236b25b9def81839bcab315cb443c98
Fernando da Costa Felício <ffelicio@autodoc.com.br>
2018-09-04 12:16:59 -0300

    Finalização do Adapter e da ViewHolder

--- app/src/main/java/com/example/recyclerviewproject/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewproject/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.recyclerviewproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.recyclerviewproject.adapter.TaskListAdapter
+import com.example.recyclerviewproject.model.Task
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = task_list_recyclerview
+        recyclerView.adapter = TaskListAdapter(tasks(), this)
+
+        val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        recyclerView.layoutManager = layoutManager
+    }
+
+    private fun tasks(): List<Task> {
+        return listOf(
+                Task("Aprender Kotlin", "Item para montar a lista de estudos da linguagem."),
+                Task("Ciclo de Vida - Activity", "Estudos das referências dos métodos utilizados no aplicativo."),
+                Task("Iniciando outra Activity", "Estudo das activities e passagem de valores entre as mesmas."),
+                Task("Criar uma simples interface (EditText)", "Estudo para criação do layout e campos de texto editáveis."),
+                Task("Criar uma interface com RecyclerView", "Estudo do RecyclerView e os componentes para criação, como Adapter e ViewHolder"),
+                Task("Principais componentes - Material Design", "Estudos dos componentes de layout")
+        )
     }
 }
--- app/src/main/java/com/example/recyclerviewproject/adapter/TaskListAdapter.kt
+++ app/src/main/java/com/example/recyclerviewproject/adapter/TaskListAdapter.kt
@@ -1,29 +1,98 @@
 package com.example.recyclerviewproject.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewproject.R
 import com.example.recyclerviewproject.model.Task
+import kotlinx.android.synthetic.main.task_item.view.*
 
-class TaskListAdapter(private val tasks: List<Task>) : RecyclerView.Adapter<TaskListAdapter.ViewHolder>() {
+/*
+ * No construtor da classe são passadas as referências pro Adapter.
+ * 1º argumento - Lista de dados a serem percorridos
+ * 2º argumento - Contexto de quem está utilizando o RecyclerView (o mesmo é passado na Activity)
+ */
+class TaskListAdapter(private val tasks: List<Task>, private val context: Context) : RecyclerView.Adapter<TaskListAdapter.ViewHolder>() {
+
+    /**
+     * Método responsável por inflar o layout.
+     * No mesmo é retornada a instância do ViewHolder. Em seu argumento é passada a view a ser inflada.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view = LayoutInflater.from(context).inflate(R.layout.task_item, parent, false)
+        return ViewHolder(view)
     }
 
-    // Retorna a quantidade de itens na lista para o adapter
+    // Retorna o tamanho da lista de itens para o adapter
     override fun getItemCount(): Int {
         return tasks.size
     }
 
+    /*
+     * Método responsável por juntar as informações contidas no ViewHolder com os componentes do layout
+     *
+     * O operador "?", durante a declaração de uma variável, parâmetro, property ou retorno de função, nos indica que existe uma possibilidade que o valor seja "null",
+     * portanto, o Kotlin exige que a gente trate esse objeto e garanta que ele não seja "null" (Null Safety).
+     * O Null Safety tem a proposta de evitar o "NullPointerException".
+     */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // Resgata o item da lista por meio da posição recebida para fazer o processo de bind (juntar as informações).
+        val task = tasks[position]
+
+        /*
+         * Abordagem 1
+         * ----------------------------------------------------------------------------------------------------------------------------------------------------
+         * Vincula a informação do objeto com os textView's
+         * ----------------------------------------------------------------------------------------------------------------------------------------------------
+         * O operador "?" a frente da propriedade significa "Safe Call".
+         * A partir dele temos a capacidade de garantir que um membro de um objeto (property, funções ou classes) vai ser chamado apenas se ele não for "null".
+        holder?.title?.text = task.title
+        holder?.description?.text = task.description
+        */
+
+        /*
+         * Abordagem 2
+         * --------------------------------------------------------------------------------------------------------------------------------------------------
+         * Função let
+         * --------------------------------------------------------------------------------------------------------------------------------------------------
+         * A mesma pode ser chamada a partir de um objeto (como é o caso do holder), e então executa um bloco de função com o objeto que a chamou embutido
+         * dentro dela.
+         * Quando foi feito a "Safe Call" e chamamos o "let" com o nosso holder, o nosso holder foi para dentro do escopo do "let" como um objeto que não é null.
+         * Portanto, ao utilizá-lo dentro do escopo do "let", não existe mais a necessidade de verificar se ele ou seus membros são "null".
+        holder?.let {
+            // Quando utilizamos o "let", o objeto enviado pode ser chamado utilizando a keyword "it". Ela é o próprio objeto embutido dentro do "let".
+            // holder.title.text = task.title
+            // holder.description.text = task.description
+            it.title.text = task.title
+            it.description.text = task.description
+        }
+        */
+
+        /*
+         * Abordagem 3
+         * --------------------------------------------------------------------------------------------------------------------------------------------------
+         * Chamando o método da ViewHolder.
+         * A responsabilidade de vínculo das informações do objeto, foi removida do método "onBindViewHolder" e foi transferida a própria classe "ViewHolder".
+         */
+        holder.bindView(task)
     }
 
-    /**
+    /*
      * Nested Class (Classe aninhada)
      * A mesma somente será utilizada para o Adapter TaskListAdapter
      */
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        /*
+         * Delega a responsabilidade de vínculo dos itens de layout às referências do objeto
+         */
+        fun bindView(task: Task) {
+            val title = itemView.textViewTitle
+            val description = itemView.textViewDescription
 
+            title.text = task.title
+            description.text = task.description
+        }
     }
 }
